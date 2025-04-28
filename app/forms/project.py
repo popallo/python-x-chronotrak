4b10c2f0f7cad6cde5575bac1954fc20de72ca02
Aurélien Dazy <aurelien.dazy@wildcodeschool.com>
2025-04-28 14:25:19 +0200

    feat(forms): add SelectField for credit addition to restrict user input
    Change `amount` field in `AddCreditForm` to SelectField to offer predefined
    time increments. This prevents incorrect input and increases usability by
    offering clear choices.
    
    style(add_credit.html): update alert style and form input to improve UI
    
    Change alert styling to `alert-warning` for better user attention and
    update form input to `form-select` for consistency with new SelectField
    implementation.

--- app/forms/project.py
+++ app/forms/project.py
@@ -1,5 +1,5 @@
 from flask_wtf import FlaskForm
-from wtforms import StringField, TextAreaField, FloatField, SubmitField
+from wtforms import StringField, TextAreaField, FloatField, SubmitField, SelectField
 from wtforms.validators import DataRequired, Length, NumberRange, Optional
 
 class ProjectForm(FlaskForm):
@@ -9,6 +9,42 @@ class ProjectForm(FlaskForm):
     submit = SubmitField('Enregistrer')
 
 class AddCreditForm(FlaskForm):
-    amount = FloatField('Heures à ajouter', validators=[DataRequired(), NumberRange(min=0.1)])
+    amount = SelectField('Heures à ajouter', validators=[DataRequired()], coerce=float)
     note = StringField('Note', validators=[Optional(), Length(max=200)])
     submit = SubmitField('Ajouter le crédit')
+    
+    def __init__(self, *args, **kwargs):
+        super(AddCreditForm, self).__init__(*args, **kwargs)
+        # Créer des options par tranches de 5 minutes jusqu'à 8 heures
+        hours_options = []
+        
+        # Ajouter les options de 5 minutes à 1 heure par incréments de 5 minutes
+        for i in range(1, 13):  # De 5 à 60 minutes par pas de 5
+            minutes = i * 5
+            decimal_value = round(minutes / 60, 2)
+            
+            if minutes < 60:
+                label = f"{minutes} min"
+            else:
+                label = "1h"
+            
+            hours_options.append((decimal_value, label))
+        
+        # Ajouter les options de 1h15 à 8h par incréments de 15 minutes
+        for i in range(5, 32):  # De 1h15 à 8h par pas de 15 minutes
+            hour = i // 4
+            minute = (i % 4) * 15
+            decimal_value = round(hour + minute / 60, 2)
+            
+            if minute > 0:
+                label = f"{hour}h {minute}min"
+            else:
+                label = f"{hour}h"
+            
+            hours_options.append((decimal_value, label))
+        
+        # Ajouter des options pour des grands blocs de temps (10h, 20h, 50h, 100h)
+        additional_options = [(10.0, "10h"), (20.0, "20h"), (50.0, "50h"), (100.0, "100h")]
+        hours_options.extend(additional_options)
+        
+        self.amount.choices = hours_options

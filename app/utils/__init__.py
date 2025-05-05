ba261dc8997698592a5ef1b1ad201efb4fa96147
Aurélien Dazy <aurelien.dazy@wildcodeschool.com>
2025-05-05 20:54:56 +0200

    feat(utils): add get_client_choices function to retrieve client list for SelectField

--- app/utils/__init__.py
+++ app/utils/__init__.py
@@ -1,6 +1,11 @@
 from datetime import datetime, timezone
 from app.utils.time_format import format_time
+from app.models.client import Client
 
 def get_utc_now():
     """Renvoie la date et l'heure actuelle en UTC avec fuseau horaire explicite"""
     return datetime.now(timezone.utc)
+
+def get_client_choices():
+    """Retourne la liste des clients pour un SelectField."""
+    return [(client.id, client.name) for client in Client.query.order_by(Client.name).all()]

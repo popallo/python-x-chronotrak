161071aa259f151e61842821280aa1ebae6fde12
Aurélien Dazy <aurelien.dazy@wildcodeschool.com>
2025-05-05 20:55:24 +0200

    feat(utils): add generate_hour_options function to create time options for SelectField

--- app/utils/time_format.py
+++ app/utils/time_format.py
@@ -18,3 +18,27 @@ def format_time(value):
             return f"{hours}h"
     else:
         return f"{minutes}min"
+
+def generate_hour_options(extra_blocks=None, include_undefined=False):
+    """
+    Génère des options horaires pour les SelectField (5min à 8h + blocs supplémentaires).
+    :param extra_blocks: liste de tuples (valeur, label) à ajouter à la fin
+    :param include_undefined: si True, ajoute '-- Non défini --' au début
+    """
+    hours_options = []
+    for i in range(1, 13):
+        minutes = i * 5
+        decimal_value = round(minutes / 60, 2)
+        label = f"{minutes} min" if minutes < 60 else "1h"
+        hours_options.append((decimal_value, label))
+    for i in range(5, 32):
+        hour = i // 4
+        minute = (i % 4) * 15
+        decimal_value = round(hour + minute / 60, 2)
+        label = f"{hour}h {minute}min" if minute > 0 else f"{hour}h"
+        hours_options.append((decimal_value, label))
+    if extra_blocks:
+        hours_options.extend(extra_blocks)
+    if include_undefined:
+        hours_options = [(0.0, '-- Non défini --')] + hours_options
+    return hours_options

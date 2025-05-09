6d1d402c660f617ce9a2205830a7c0d55b796fbd
Aurélien Dazy <aurelien.dazy@wildcodeschool.com>
2025-05-09 18:26:18 +0200

    feat(admin): update TimeTransferForm to use SelectField for hours and enhance amount choices with dynamic options

--- app/forms/admin.py
+++ app/forms/admin.py
@@ -1,7 +1,8 @@
 from flask_wtf import FlaskForm
 from wtforms import StringField, TextAreaField, FloatField, SelectField, BooleanField, SubmitField
-from wtforms.validators import DataRequired, Email, Optional, NumberRange
+from wtforms.validators import DataRequired, Email, Optional, NumberRange, Length
 from app.models.project import Project
+from app.utils.time_format import generate_hour_options
 
 class TestEmailForm(FlaskForm):
     recipient = StringField('Destinataire', validators=[DataRequired(), Email()])
@@ -12,13 +13,13 @@ class TestEmailForm(FlaskForm):
 class TimeTransferForm(FlaskForm):
     source_project_id = SelectField('Projet source', coerce=int, validators=[DataRequired()])
     target_project_id = SelectField('Projet destination', coerce=int, validators=[DataRequired()])
-    amount = FloatField('Montant (heures)', validators=[
-        DataRequired(),
-        NumberRange(min=0.1, message="Le montant doit être supérieur à 0")
-    ])
+    amount = SelectField('Montant (heures)', validators=[DataRequired()], coerce=float)
     description = TextAreaField('Description', validators=[Optional()])
     submit = SubmitField('Transférer le temps')
 
     def __init__(self, *args, **kwargs):
         super(TimeTransferForm, self).__init__(*args, **kwargs)
         # Les projets seront chargés dynamiquement dans la route
+        self.amount.choices = generate_hour_options(
+            extra_blocks=[(10.0, "10h"), (20.0, "20h"), (50.0, "50h"), (100.0, "100h")]
+        )

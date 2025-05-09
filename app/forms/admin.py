028eeb6cfb9edc4a3d1e025aee615292b8f56e92
Aurélien Dazy <aurelien.dazy@wildcodeschool.com>
2025-05-09 18:19:04 +0200

    feat(admin): add time transfer functionality and enhance email form with subject and message fields

--- app/forms/admin.py
+++ app/forms/admin.py
@@ -1,7 +1,24 @@
 from flask_wtf import FlaskForm
-from wtforms import StringField, SubmitField
-from wtforms.validators import DataRequired, Email
+from wtforms import StringField, TextAreaField, FloatField, SelectField, BooleanField, SubmitField
+from wtforms.validators import DataRequired, Email, Optional, NumberRange
+from app.models.project import Project
 
 class TestEmailForm(FlaskForm):
-    recipient = StringField('Adresse email de test', validators=[DataRequired(), Email()])
-    submit = SubmitField('Envoyer un email de test')
+    recipient = StringField('Destinataire', validators=[DataRequired(), Email()])
+    subject = StringField('Sujet', validators=[DataRequired()])
+    message = TextAreaField('Message', validators=[DataRequired()])
+    submit = SubmitField('Envoyer')
+
+class TimeTransferForm(FlaskForm):
+    source_project_id = SelectField('Projet source', coerce=int, validators=[DataRequired()])
+    target_project_id = SelectField('Projet destination', coerce=int, validators=[DataRequired()])
+    amount = FloatField('Montant (heures)', validators=[
+        DataRequired(),
+        NumberRange(min=0.1, message="Le montant doit être supérieur à 0")
+    ])
+    description = TextAreaField('Description', validators=[Optional()])
+    submit = SubmitField('Transférer le temps')
+
+    def __init__(self, *args, **kwargs):
+        super(TimeTransferForm, self).__init__(*args, **kwargs)
+        # Les projets seront chargés dynamiquement dans la route
